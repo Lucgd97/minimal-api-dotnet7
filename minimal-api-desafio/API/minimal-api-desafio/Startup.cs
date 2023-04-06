e6442ea9a02f0dc0cb4ce705a04c8074ba7c827a
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-04-06 19:22:09 -0300

    Correção de implementação startup

--- minimal-api-desafio/API/minimal-api-desafio/Startup.cs
+++ minimal-api-desafio/API/minimal-api-desafio/Startup.cs
@@ -1,8 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using MinimalApiDesafio.ModelViews;
-using MinimalApiDesafio.Models;
 using MinimalApiDesafio.DTOs;
+using MinimalApiDesafio.Models;
 using Microsoft.OpenApi.Models;
+using minimal_api_desafio.Infraestrutura.Database;
+using Microsoft.EntityFrameworkCore;
+using MinimalApiDesafio.Infraestrutura.Interfaces;
+using MinimalApiDesafio.Servicos;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using MinimalApiDesafio.Routes;
 
 namespace MinimalApiDesafio;
 
@@ -13,18 +22,102 @@ public class Startup
         Configuration = configuration;
     }
 
-    public IConfiguration Configuration{ get; }
+    public IConfiguration? Configuration { get;set; }
+    public static string? StrConexao(IConfiguration? Configuration = null)
+    {
+        // === Dá prioridade ao app settings ===
+        // if(Configuration is not null)
+        // {
+        //     return Configuration?.GetConnectionString("Conexao");
+        // }
 
-    public void ConfigurationServices(IServiceCollection services)
+        // return Environment.GetEnvironmentVariable("DATABASE_URL_MINIMAL_API");
+
+        // === Dá prioridade a variavel de ambiente ===
+        string? conexao = Environment.GetEnvironmentVariable("DATABASE_URL_MINIMAL_API");
+        if(conexao is null)
+        {
+            conexao = Configuration?.GetConnectionString("Conexao");
+        }
+
+        return conexao;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
-        services.AddSwaggerGen(c =>
+        services.AddEndpointsApiExplorer();
+
+        var conexao = StrConexao(Configuration);
+        services.AddDbContext<DbContexto>(options =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal Api Desafio", Version = "v1"});
+            options.UseMySql(conexao, ServerVersion.AutoDetect(conexao));
         });
 
-        builder.Services.AddEndpointsApiExplorer();
-        //services.AddScoped<IStudentsService>, StudentesService();
+        services.AddScoped<IBancoDeDadosServico<Cliente>, ClientesServico>();
+        services.AddScoped<ILogin<Administrador>, AdministradoresServico>();
+
+        TokenServico.Secret = Configuration?["SecretJwt"] ?? "";
+        var key = Encoding.ASCII.GetBytes(TokenServico.Secret);
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("administrador", policy => policy.RequireClaim("administrador"));
+            options.AddPolicy("editor", policy => policy.RequireClaim("editor"));
+        });
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Minimal API",
+                Description = "Torne-se um programador API Minima",
+                Contact = new OpenApiContact { Name = "Danilo Aparecido", Email = "suporte@torneseumprogramador.com.br" },
+                License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+            });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "Insira o token JWT como no exemplo: Bearer {SEU_TOKEN}",
+                Name = "Authorization",
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,124 +125,26 @@ public class Startup
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal Api Desafio v1"));
         }
 
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
         app.UseHttpsRedirection();
-        
+
         app.UseRouting();
+
+        app.UseAuthentication();
 
         app.UseAuthorization();
 
-        app.MapControllers();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
 
-        MapRoutes(app);
-        MapRoutesClientes(app);
+            HomeRoute.MapRoutes(endpoints);
+            AdministradoresRoute.MapRoutes(endpoints);
+            ClientesRoute.MapRoutes(endpoints);
+        });
     }
-
-    
-    #region Rotas utilizando Mininal API
-
-    public void MapRoutes(WebApplication app)
-    {
-        app.MapGet("/", () => new {Mensagem = "Bem vindo a Api"})// rota minima para api
-        .Produces<dynamic>(StatusCodes.Status200OK)
-        .WithName("Home")
-        .WithTags("Testes"); 
-
-
-        app.MapGet("/recebe-parametro", (HttpRequest request, HttpResponse response, string? nome) =>
-        {
-
-            if(string.IsNullOrEmpty(nome))
-            {
-                return Results.BadRequest(new{
-                    Mensagem = "Olha você não mandou uma informação importante, o nome é obrigatório"
-                });
-                
-            }
-
-            nome = $""" 
-            Alterando parametro recebido {nome}
-            """;
-
-            var objetoDeRetorno = new {
-                ParametroPassado = nome,
-                Mensagem = "Muito bem alunos passamos um parametro por querystring"
-            };
-
-            return Results.Created($"/recebe-parametro/{objetoDeRetorno.ParametroPassado}", objetoDeRetorno);
-        })
-        .Produces<dynamic>(StatusCodes.Status201Created)
-        .Produces(StatusCodes.Status400BadRequest)
-        .WithName("TesteRecebeParametro")
-        .WithTags("Testes");
-    }
-
-    public void MapRoutesClientes(WebApplication app) 
-    {
-        // rota Get - retornar informação
-        app.MapGet("/clientes", () => 
-        {
-            var clientes = new List<Cliente>();
-            // var clientes = ClienteService.Todos();
-            return Results.Ok(clientes);
-        })
-        .Produces<Cliente>(StatusCodes.Status200OK)
-        .WithName("GetClientes")
-        .WithTags("Clientes");
-
-        // rota post
-        app.MapPost("/clientes", ([FromBody] ClienteDTO clienteDTO) => 
-        {
-            var cliente = new Cliente
-            {
-                Nome = clienteDTO.Nome,
-                Telefone = clienteDTO.Telefone,
-                Email = clienteDTO.Email,
-            };
-            // cliente.salvar(cliente)
-
-            return Results.Created($"/cliente/{cliente.Id}", cliente);
-        })
-        .Produces<dynamic>(StatusCodes.Status201Created)
-        .Produces<Error>(StatusCodes.Status400BadRequest)
-        .WithName("PostClientes")
-        .WithTags("Clientes");
-
-        // rota put - alterar
-        app.MapPut("/clientes/{id}", ([FromRoute] int id, [FromBody] ClienteDTO clienteDTO) => 
-        {
-
-            if(!string.IsNullOrEmpty(clienteDTO.Nome))
-            {
-                return Results.BadRequest(new Error
-                {
-                    Codigo = 123432,
-                    Mensagem = "Nome é obrigatório"
-                });
-            }
-
-            /*var cliente = ClienteService.BuscarPorId(id);
-            if(cliente == null)
-                return Results.NotFound(new Error {Codigo = 123432, Mensagem = "Você passou um cliente inexistente"});
-
-            cliente.Nome = clienteDTO.Nome;
-            cliente.Telefone = clienteDTO.Telefone;
-            cliente.Email = clienteDTO.Email;
-            ClienteService.Update(cliente);*/    
-
-            var cliente = new Cliente();
-        
-            return Results.Ok(cliente);
-        })
-        .Produces<Cliente>(StatusCodes.Status200OK)
-        .Produces<Error>(StatusCodes.Status404NotFound)
-        .Produces<Error>(StatusCodes.Status400BadRequest)
-        .WithName("PutClientes")
-        .WithTags("Clientes");
-    }
-
-    #endregion
 }
