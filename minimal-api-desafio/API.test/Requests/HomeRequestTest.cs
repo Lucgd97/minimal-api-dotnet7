4a694361014422a0f965162f36624af6be74fec2
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-04-08 17:40:57 -0300

    Implementando testes novos

--- minimal-api-desafio/API.test/Requests/HomeRequestTest.cs
+++ minimal-api-desafio/API.test/Requests/HomeRequestTest.cs
@@ -1,19 +1,36 @@
 using System.Net;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using MinimalApiDesafio;
 
 namespace API.test.Requests;
 
 [TestClass]
 public class HomeResquestTest
 {
+    public const string PORT = "5001";
+    public const string HOST = "http://localhost"; 
     private static TestContext _testContext;
-    private static WebApplicationFactory<Startup> _factory;
+    private static WebApplicationFactory<Startup> _http;
+
+    [ClassInitialize]
+    public static void ClassInit(TestContext testContext)
+    {
+        _testContext = testContext;
+        _http = new WebApplicationFactory<Startup>();
+    }
 
     [TestMethod]
     public async Task TestaSeAHomeDaAPIExiste()
     {
-        HttpClient cli = new HttpClient();
-        var request = await cli.GetAsync("http://localhost:5287");
+
+        _http = _http.WithWebHostBuilder(builder =>{
+            
+            builder.UseSetting("https_port", PORT).UseEnvironment("Testing");
+        });
+
+        var cli = _http.CreateClient();
+        var request = await cli.GetAsync($"{HOST}:{PORT}");
 
         Assert.AreEqual(HttpStatusCode.OK, request.StatusCode);
     }
