450de948f75be2350c0406ec545841dd14505d72
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-04-10 07:14:29 -0300

    Correcao de bug de versao e implementando testes novos

--- minimal-api-desafio/API.test/Requests/HomeRequestTest.cs
+++ minimal-api-desafio/API.test/Requests/HomeRequestTest.cs
@@ -12,12 +12,20 @@ public class HomeResquestTest
     public const string HOST = "http://localhost"; 
     private static TestContext _testContext = default!;
     private static WebApplicationFactory<Startup> _http = default!;
+    private static HttpClient _client = default!;
 
     [ClassInitialize]
     public static void ClassInit(TestContext testContext)
     {
         _testContext = testContext;
         _http = new WebApplicationFactory<Startup>();
+
+        _http = _http.WithWebHostBuilder(builder =>{
+            
+            builder.UseSetting("https_port", PORT).UseEnvironment("Testing");
+        });
+
+        _client = _http.CreateClient();
     }
 
     [ClassCleanup]
@@ -29,15 +37,24 @@ public class HomeResquestTest
     [TestMethod]
     public async Task TestaSeAHomeDaAPIExiste()
     {
-
-        _http = _http.WithWebHostBuilder(builder =>{
-            
-            builder.UseSetting("https_port", PORT).UseEnvironment("Testing");
-        });
-
-        var client = _http.CreateClient();
-        var response = await client.GetAsync("/");
+        var response = await _client.GetAsync("/");
 
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.AreEqual("""{"mensagem":"Bem vindo a API"}""", result);
+    }
+
+    [TestMethod]
+    public async Task TestandoCaminhoFelizParaReceberParametro()
+    {
+        var response = await _client.GetAsync("/");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.AreEqual("""{"mensagem":"Bem vindo a API"}""", result);
     }
 }
