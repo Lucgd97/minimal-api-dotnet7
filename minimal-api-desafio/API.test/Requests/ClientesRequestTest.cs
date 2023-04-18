981cd74eb39c6a88aff84104266433757aede730
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-04-18 18:58:19 -0300

    Terminando de implementar testes post, get e put

--- minimal-api-desafio/API.test/Requests/ClientesRequestTest.cs
+++ minimal-api-desafio/API.test/Requests/ClientesRequestTest.cs
@@ -1,8 +1,12 @@
 using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 using API.Test.Helpers;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using MinimalApiDesafio;
+//using API.Test.Mock;
+using MinimalApiDesafio.DTOs;
+using MinimalApiDesafio.Models;
+//using MinimalApiDesafio.Servicos
 
 namespace API.test.Requests;
 
@@ -24,38 +28,102 @@ public class ClientesResquestTest
 
     [TestMethod]
     public async Task GetClientes()
-    {
+    {   
+        await SetHeaderToken();
         var response = await Setup.client.GetAsync("/clientes");
 
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
 
         var result = await response.Content.ReadAsStringAsync();
-        Assert.AreEqual("""[]""", result);
+        var clientes = JsonSerializer.Deserialize<List<Cliente>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(clientes);
+        Assert.IsTrue(clientes.Count > 0);
+        Assert.IsNotNull(clientes[0].Id);
+        Assert.IsNotNull(clientes[0].Nome);
+        Assert.IsNotNull(clientes[0].Email);
+        Assert.IsNotNull(clientes[0].Telefone);
+        Assert.IsNotNull(clientes[0].DataCriacao);
     }
+    
 
     [TestMethod]
     public async Task PostClientes()
     {
-        var response = await Setup.client.PostAsync("/clientes");
+        await SetHeaderToken();
+        var cliente = new ClienteDTO()
+        {
+            Nome = "Janaina",
+            Email = "jan@gmail.com",
+            Telefone = "(11)11111-11111"
+        };
 
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        var content = new StringContent(JsonSerializer.Serialize(cliente), Encoding.UTF8, "application/json");
+        var response = await Setup.client.PostAsync("/clientes", content);
+
+        Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
         Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
 
         var result = await response.Content.ReadAsStringAsync();
-        Assert.AreEqual("""[]""", result);
+        var clienteResponse = JsonSerializer.Deserialize<Cliente>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(clienteResponse);
+        Assert.AreEqual(1, clienteResponse.Id);
     }
 
     [TestMethod]
     public async Task PutClientes()
     {
-        var response = await Setup.client.PostAsync("/clientes");
+        await SetHeaderToken();
+        var cliente = new ClienteDTO()
+        {
+            Nome = "Janaina",
+            Email = "jan@gmail.com",
+            Telefone = "(11)11111-11111"
+        };
+
+        var content = new StringContent(JsonSerializer.Serialize(cliente), Encoding.UTF8, "application/json");
+        var response = await Setup.client.PutAsync($"/clientes/{1}", content);
 
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
 
         var result = await response.Content.ReadAsStringAsync();
-        Assert.AreEqual("""[]""", result);
+        var clienteResponse = JsonSerializer.Deserialize<Cliente>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(clienteResponse);
+        Assert.AreEqual(1, clienteResponse.Id);
+        Assert.AreEqual("Janaina", clienteResponse.Nome);
+    }
+
+    [TestMethod]
+    public async Task PutClientesSemNome()
+    {
+        await SetHeaderToken();
+        var cliente = new ClienteDTO()
+        {
+            Email = "jan@gmail.com",
+            Telefone = "(11)11111-11111"
+        };
+
+        var content = new StringContent(JsonSerializer.Serialize(cliente), Encoding.UTF8, "application/json");
+        var response = await Setup.client.PutAsync($"/clientes/{1}", content);
+
+        Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.AreEqual("""{"codigo":123432,"mensagem":"O Nome é obrigatório"}""", result);
     }
 
     
