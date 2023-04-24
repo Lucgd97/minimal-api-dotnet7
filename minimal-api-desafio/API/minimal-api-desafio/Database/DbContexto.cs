0ef99ef1e845a773340b55c48755b76ff158fc4a
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-04-24 07:12:10 -0300

    Implementando Entity Annotation

--- minimal-api-desafio/API/minimal-api-desafio/Database/DbContexto.cs
+++ minimal-api-desafio/API/minimal-api-desafio/Database/DbContexto.cs
@@ -1,1 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApiDesafio.Models;
+
 namespace minimal_api_desafio.Database;
+
+public class DbContexto : DbContext
+{
+    // Entities
+    public DbSet<Cliente> Clientes { get; set; } = default!;
+}
--- minimal-api-desafio/API/minimal-api-desafio/Models/Cliente.cs
+++ minimal-api-desafio/API/minimal-api-desafio/Models/Cliente.cs
@@ -1,14 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MinimalApiDesafio.Models;
 
+[Table("clientes")]
 public record Cliente
 {
     public Cliente()
     {
         this.DataCriacao = DateTime.Now;
     }
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id {get; set;}
-    public string? Nome {get; set;}
-    public string? Telefone {get; set;}
+
+    public string Nome {get; set;} = default!;
+
+    [MaxLength(20)]
+    public string Telefone {get; set;} = default!;
+
     public string? Email {get; set;}
+    
     public DateTime DataCriacao { get;set; }
 }
